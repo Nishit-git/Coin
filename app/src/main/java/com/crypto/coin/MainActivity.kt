fbbf89fbe3957dcc083ec293dbedec95487eaab6
Nish <nishit.bhuchar100@gmail.com>
2025-06-27 23:02:29 -0400

    Add project wide git ignore

--- app/src/main/java/com/crypto/coin/MainActivity.kt
+++ app/src/main/java/com/crypto/coin/MainActivity.kt
@@ -3,7 +3,6 @@ package com.crypto.coin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,7 @@ import com.crypto.coin.ui.theme.CoinTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             CoinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
